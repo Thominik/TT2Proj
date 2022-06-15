75cad6d6cd7e674c9933518f3001681ada56ee41
Thominik <dominikbednarz.ti@gmail.com>
2022-06-15 14:11:00 +0200

    Frontend done

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,16 +7,13 @@ import {NotFoundComponent} from "./core/not-found/not-found.component";
 import {AuthGuard} from "./core/guards/auth.guard";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
-  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
-  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Errors'}},
-  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Strona główna'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
-    data: {breadcrumb: 'Shop'}},
+    data: {breadcrumb: 'Sklep'}},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
-    data: {breadcrumb: 'Basket'}},
+    data: {breadcrumb: 'Koszyk'}},
   {path: 'checkout', canActivate: [AuthGuard], loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule),
-    data: {breadcrumb: 'Checkout'}},
+    data: {breadcrumb: 'Podgląd'}},
   {path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule),
     data: {breadcrumb: {skip: true}}},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
